62790df818af5a9dc0f35faf7ed1d10807383186
T C Divyashree <137554058+divyashree029@users.noreply.github.com>
2026-06-15 22:25:25 +0530

    Fix dependency

--- script.py
+++ script.py
@@ -63,7 +63,7 @@ def get_text_chunks(text):
 def get_vector_store(text_chunks):
 
     embeddings = GoogleGenerativeAIEmbeddings(
-        model="models/text-embedding-004",
+        model="models/embedding-001",
         google_api_key=os.getenv("GOOGLE_API_KEY")
     )
 
